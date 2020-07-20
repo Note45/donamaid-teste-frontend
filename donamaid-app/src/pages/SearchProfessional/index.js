40c64ee4d973a3f7ff64fc9f8339c93533c5d6e6
Note45 <antonion.2048@gmail.com>
2020-07-20 15:37:10 -0300

    Created input and button search to page SearchProfessional

--- donamaid-app/src/pages/SearchProfessional/index.js
+++ donamaid-app/src/pages/SearchProfessional/index.js
@@ -9,6 +9,11 @@ const SearchProfessional = () => {
       <header>
         <img id='logo' src={logo} alt='Logo Donamaid'/>
       </header>
+
+      <div id='searchContainer'>
+        <input type='text' placeholder='Digite o nome do profissional...'/>
+        <button>Pesquisar Profissional</button>
+      </div>
     </div>
   );
 }
