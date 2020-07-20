8b2efb25f0048c6aa5c64de682c2448ddcdd66dd
Note45 <antonion.2048@gmail.com>
2020-07-20 19:36:17 -0300

    Created base style to Profile Page

--- donamaid-app/src/pages/ProfilePage/index.js
+++ donamaid-app/src/pages/ProfilePage/index.js
@@ -3,16 +3,38 @@ import { useLocation } from 'react-router-dom';
 
 import './styles.css';
 import logo from '../../assets/donamaid-branco-logo.png';
+import profileAvatar from '../../assets/profile-avatar.png';
 
 const ProfilePage = () => {
   const professionalData = useLocation().state;
 
   return(
-    <div id='profileContainer'>
+    <div id='profilePageContainer'>
       <header>
         <img id='logo' src={logo} alt='Logo Donamaid'/>
       </header>
 
+      <div id='professionalProfile'>
+        <img id='avatar' src={profileAvatar} alt='Foto de perfil do candidato'/>
+
+        <div id='professionalInformation'>
+          <div id='professionalName'>
+            <h1>Nome do Prosissional</h1>
+            <legend>Com a Donamaid desde 2018</legend>
+          </div>
+
+
+          <p>Description Description Description Description Description
+          Description Description Description Description Description
+          Description Description Description Description Description
+          Description Description Description Description Description
+          </p>
+        </div>
+      </div>
+      
+      <div id='containerButtonHire'>
+        <button id='hireProfessional'>QUERO CONTRATAR NAME</button>
+      </div>
     </div>
   );
 }
