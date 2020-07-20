54b0765e346bcf5c3aa0f5bae7ad5f49a62161c0
Note45 <antonion.2048@gmail.com>
2020-07-20 15:09:09 -0300

    Created header to page SearchProfessional

--- donamaid-app/src/pages/SearchProfessional/index.js
+++ donamaid-app/src/pages/SearchProfessional/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import './styles.css';
+import logo from '../../assets/donamaid-branco-logo.png';
 
 const SearchProfessional = () => {
   return (
     <div id='containerSearchProfessional'>
-      <h1>Page Search Professional</h1>
+      <header>
+        <img id='logo' src={logo} alt='Logo Donamaid'/>
+      </header>
     </div>
   );
 }
